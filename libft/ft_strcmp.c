e62b3266e114b4ee071667c1b7351368f69b5a6a
lazrossi <lazrossi@student.42.fr>
2017-09-07 19:36:17 +0200

    MAJ

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -17,11 +17,6 @@ int		ft_strcmp(const char *s1, const char *s2)
 	int i;
 
 	i = 0;
-	ft_putchar('\n');
-	ft_putstr((char*)s2);
-	ft_putchar(' ');
-	ft_putstr((char*)s1);
-	ft_putchar('\n');
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
