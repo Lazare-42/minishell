0504e3075f7c65848d6f81db5bc128fc515868c7
lazrossi <lazrossi@student.42.fr>
2017-09-22 16:47:11 +0800

    ft_echo fini

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -1,16 +1,55 @@
+char extern **environ;
+
 #include "../includes/minishell.h"
+
+void	ft_print_corresponding_variable(char *str)
+{
+	size_t	i;
+	int		j;
+
+	i = ft_strlen(str);
+	j = 0;
+	str++;
+	if (str)
+	{
+		while (environ[j] && (ft_memcmp(environ[j], str, i - 1) || environ[j][i - 1] != '='))
+			j++;
+		if (environ[j])
+			ft_putstr(&environ[j][i]);
+	}
+}
+
+
+
+void	ft_parse(char **str)
+{
+	int i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (str[i][j] && str[i][j] != '$')
+		{
+			ft_putchar(str[i][j]);
+			j++;
+		}
+		if (str[i][j] && str[i][j] == '$')
+			ft_print_corresponding_variable(&str[i][j]);
+		ft_putchar(' ');
+		i++;
+	}
+}
 
 void	ft_echo(char **str, int quote_presence)
 {
 	quote_presence++;
-	if (str)
+	if (!(ft_memcmp(str[0], "-n", 2)))
+		ft_parse(&str[1]);
+	else
 	{
-		if (!(ft_memcmp(str[0], "-n", 2)))
-			ft_putabs(&str[1], ' ');
-		else
-		{
-			ft_putabs(str, ' ');
-			ft_putchar('\n');
-		}
+		ft_parse(str);
+		ft_putchar('\n');
 	}
 }
