90d11e500380a7cad64da3da4d4403d29e3feea8
Lazare ROSSILLON <lazrossi@e1r5p6.42.fr>
2017-12-29 11:45:33 +0100

    MAJ avant test dune commande

--- libft/ft_putabs.c
+++ libft/ft_putabs.c
@@ -6,7 +6,7 @@
 /*   By: lazrossi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/05 09:29:17 by lazrossi          #+#    #+#             */
-/*   Updated: 2017/09/06 12:44:47 by lazrossi         ###   ########.fr       */
+/*   Updated: 2017/12/29 11:40:45 by lazrossi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_putabs(char **str, char separator)
 {
 	int i;
 
-	i = 0;
+	i = 2;
 	while (str && str[i])
 	{
 		ft_putstr(str[i]);
